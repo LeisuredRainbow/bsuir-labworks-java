43e32ac1ab78f90a31dc046bd99c39f515a7d7c3
Dominik <anonimer287@mail.ru>
2026-02-22 20:05:09 +0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/by/bsuir/labworks/LabworksApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.labworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/labworks/LabworksApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.labworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
