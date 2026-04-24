c0dd9a90d4696cdc9d3126bda11c76c02fc225ff
Dominik <anonimer287@mail.ru>
2026-04-24 07:08:18 +0300

    LAB3 - DONE

--- src/main/java/by/bsuir/labworks/cache/BookingSearchCache.java
+++ src/main/java/by/bsuir/labworks/cache/BookingSearchCache.java
@@ -11,20 +11,14 @@ public class BookingSearchCache {
   private final Map<BookingSearchKey, Page<BookingResponseDto>> cache = new HashMap<>();
 
   public Page<BookingResponseDto> get(BookingSearchKey key) {
-    synchronized (cache) {
-      return cache.get(key);
-    }
+    return cache.get(key);
   }
 
   public void put(BookingSearchKey key, Page<BookingResponseDto> value) {
-    synchronized (cache) {
-      cache.put(key, value);
-    }
+    cache.put(key, value);
   }
 
   public void invalidateAll() {
-    synchronized (cache) {
-      cache.clear();
-    }
+    cache.clear();
   }
 }
