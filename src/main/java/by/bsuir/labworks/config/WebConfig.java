1fb7a585ddceb897dd2959a0b155e20e91612779
Dominik <anonimer287@mail.ru>
2026-05-12 03:41:17 +0300

    ALL DONE

--- src/main/java/by/bsuir/labworks/config/WebConfig.java
+++ src/main/java/by/bsuir/labworks/config/WebConfig.java
@@ -2,10 +2,20 @@ package by.bsuir.labworks.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableSpringDataWebSupport(
     pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 )
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        .allowedHeaders("*");
+  }
 }
